a23f6d1e2bd68b6d4cb37989e3eee4757c26506b
Ruben Jacob <97407000+Ruben-Jacob@users.noreply.github.com>
2024-07-01 09:34:34 +0530

    Time: 2139 ms (33.46%), Space: 12.4 MB (88.29%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+solution = Solution()
+print(solution.twoSum([2, 7, 11, 15], 9))
