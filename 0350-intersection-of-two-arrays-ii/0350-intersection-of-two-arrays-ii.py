a93e075b669708ff3af89f0a40c6979d45d9b6ce
Ruben Jacob <97407000+Ruben-Jacob@users.noreply.github.com>
2024-07-02 08:05:34 +0530

    Time: 27 ms (71.36%), Space: 11.7 MB (80.34%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        count = {}
+        for num in nums1:
+            count[num] = count.get(num,0) + 1
+        intersection = []
+        for num in nums2:
+            if num in count and count[num] > 0:
+                intersection.append(num)
+                count[num] -= 1
+        return intersection
+                
+solution = Solution()
+print(solution.intersect([1,2,2,1],[2,2]))
