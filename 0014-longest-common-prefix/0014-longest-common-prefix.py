2830ec8d003302daf9a23c05a7e1b3cbf3b1059e
Ruben Jacob <97407000+Ruben-Jacob@users.noreply.github.com>
2024-07-03 10:21:15 +0530

    Time: 11 ms (91.64%), Space: 11.9 MB (18.61%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for s in strs[1:]:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        
+        return prefix
+solution = Solution()
+print(solution.longestCommonPrefix(["flower","flow","flight"]))  
+
+        
